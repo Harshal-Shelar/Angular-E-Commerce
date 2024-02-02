ba39b225c16665cd86dba4132a047cb0e3d78a0e
Harshal-creator <harshalshelar8251@gmail.com>
2024-02-02 15:40:56 +0530

    added all components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CartPageComponent } from './components/cart-page/cart-page.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { MyOrdersComponent } from './components/my-orders/my-orders.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { SearchComponent } from './components/search/search.component';
+import { SellerAddProductComponent } from './components/seller-add-product/seller-add-product.component';
+import { SellerAuthComponent } from './components/seller-auth/seller-auth.component';
+import { SellerHomeComponent } from './components/seller-home/seller-home.component';
+import { SellerUpdateProductComponent } from './components/seller-update-product/seller-update-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartPageComponent,
+    CheckoutComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomeComponent,
+    MyOrdersComponent,
+    ProductDetailsComponent,
+    SearchComponent,
+    SellerAddProductComponent,
+    SellerAuthComponent,
+    SellerHomeComponent,
+    SellerUpdateProductComponent
   ],
   imports: [
     BrowserModule,
